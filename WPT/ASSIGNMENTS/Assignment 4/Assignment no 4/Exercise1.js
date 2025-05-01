d6732f9db97d13f572a8b8eb3ff4bc429d9321f7
ShitalHiray <142962376+ShitalHiray@users.noreply.github.com>
2025-05-01 16:52:40 +0530

    Update Exercise1.js

--- WPT/ASSIGNMENTS/Assignment 4/Assignment no 4/Exercise1.js
+++ WPT/ASSIGNMENTS/Assignment 4/Assignment no 4/Exercise1.js
@@ -1,3 +1,6 @@
+/*Exercise 1: Create a function processData that takes two parameters: a string and a callback function. Your task is to write a callback that converts the string to uppercase and then call it within processData.
+    Requirements: ● Define a function toUpperCase that will serve as a callback. ● Pass a string and toUpperCase to processData and log the output.*/
+
 function toUpperCase(str) {
     return str.toUpperCase();
 }
