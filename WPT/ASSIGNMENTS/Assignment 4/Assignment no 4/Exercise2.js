7e495f6faf6d98afad0c3f7ecd9793d61eb219cb
ShitalHiray <142962376+ShitalHiray@users.noreply.github.com>
2025-05-01 16:53:51 +0530

    Update Exercise2.js

--- WPT/ASSIGNMENTS/Assignment 4/Assignment no 4/Exercise2.js
+++ WPT/ASSIGNMENTS/Assignment 4/Assignment no 4/Exercise2.js
@@ -1,3 +1,7 @@
+/*Exercise 2: Write a function forEachElement that accepts an array and a callback. 
+This function should apply the callback to each element of the array. Requirements: 
+● Pass an anonymous function as the callback that multiplies each element by 2 and logs the result with the index.*/
+
 function forEachElement(arr, callback) {
     for (let i = 0; i < arr.length; i++) {
         callback(arr[i], i);
