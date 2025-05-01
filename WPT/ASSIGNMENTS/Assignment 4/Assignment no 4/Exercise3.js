0cacc118af9a5f5a1d1e258bca289dfc343bae47
ShitalHiray <142962376+ShitalHiray@users.noreply.github.com>
2025-05-01 17:04:33 +0530

    Update Exercise3.js

--- WPT/ASSIGNMENTS/Assignment 4/Assignment no 4/Exercise3.js
+++ WPT/ASSIGNMENTS/Assignment 4/Assignment no 4/Exercise3.js
@@ -1,3 +1,6 @@
+/*Exercise 3: Simulate a network request by creating a function fetchData that takes a URL and a callback as parameters. 
+Use setTimeout to simulate a delay and then call the callback with a string representing a response. Requirements: 
+● After a delay, log the “response” to the console.*/
 function fetchData(url, callback) {
     console.log(`Fetching data from ${url}...`);
     setTimeout(() => {
